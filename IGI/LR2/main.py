fd9e7aac6d9ec57c41415eaccf53d61f0591a711
Tanya Kozlova <tanya120606@mail.ru>
2025-02-18 19:31:38 +0300

    3 task completed

---
+++ IGI/LR2/main.py
@@ -0,0 +1,8 @@
+import os
+from geometric_lib import circle, square
+
+radius = float(os.getenv("RADIUS", 5))
+print(f"Circle area: {circle.area(radius)}")
+
+side = float(os.getenv("SIDE", 4))
+print(f"Square area: {square.area(side)}")
